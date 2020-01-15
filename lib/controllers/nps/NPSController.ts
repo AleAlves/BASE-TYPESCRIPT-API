c94fc255fbcc6d7c5ea5cc20154cd38cce4aa89d
João Gabriel de Medeiros Pereira <gah.mp1@gmail.com>
2020-01-15 19:30:00 -0300

    added Todo validation if nps already exist

--- lib/controllers/nps/NPSController.ts
+++ lib/controllers/nps/NPSController.ts
@@ -13,12 +13,27 @@ export class NPSController extends BaseController {
         let nps = req.body
         nps.versionApp = NPSController.transformVersionToIntWithPadding(nps.versionApp)
         let newNPS = new NPS(req.body);
-        newNPS.save((err, nps) => {
-            if (err) {
-                super.send(res, null, err.name, 400, err.message);
-            }
-            super.send(res, nps, 'Success', 200, 'Obrigado pela avaliação!');
-        });
+
+       //TODO: NPS.findById(req.params.npsID, (err, nps) => {
+            // if (err) {
+            //     super.send(res, null, err.name, 400, err.message);
+            newNPS.save((err, nps) => {
+                if (err) {
+                    super.send(res, null, err.name, 400, err.message);
+                }
+                super.send(res, nps, 'Success', 200, 'Obrigado pela avaliação!');
+            });
+            // } else {
+         //   super.send(res, nps, null, 200);
+        //  NPS.findOneAndUpdate({ _id: req.params.npsID }, req.body, { new: true }, (err, nps) => {
+        //     if (err) {
+        //         super.send(res, null, err.name, 400, err.message);
+        //     }
+        //     super.send(res, nps, 'Success', 200, 'Obrigado pela avaliação!');
+        // });
+        //}
+        //});
+       
     }
 
     public static transformVersionToIntWithPadding(version: string) : Number {
@@ -52,7 +67,7 @@ export class NPSController extends BaseController {
     public getNPS(req: Request, res: Response) {
         NPS.find({}, (err, nps) => {
             if (err) {
-                super.send(res, null, 'Error', 400, err);
+                super.send(res, null, err.name, 400, err.message);
             }
             super.send(res, nps, null, 200);
         });
@@ -61,7 +76,7 @@ export class NPSController extends BaseController {
     public getNPSWithID(req: Request, res: Response) {
         NPS.findById(req.params.npsID, (err, nps) => {
             if (err) {
-                super.send(res, null, 'Error', 400, err);
+                super.send(res, null, err.name, 400, err.message);
             }
             super.send(res, nps, null, 200);
         });
@@ -70,7 +85,7 @@ export class NPSController extends BaseController {
     public updateNPS(req: Request, res: Response) {
         NPS.findOneAndUpdate({ _id: req.params.npsID }, req.body, { new: true }, (err, nps) => {
             if (err) {
-                super.send(res, null, 'Error', 400, err);
+                super.send(res, null, err.name, 400, err.message);
             }
             super.send(res, nps, 'Success', 200, 'Obrigado pela avaliação!');
         });
@@ -79,44 +94,10 @@ export class NPSController extends BaseController {
     public deleteNPS(req: Request, res: Response) {
         NPS.remove({ _id: req.params.npsID }, (err, nps) => {
             if (err) {
-                super.send(res, null, 'Error', 400, err);
+                super.send(res, null, err.name, 400, err.message);
             }
             super.send(res, nps, null, 200);
         });
     }
-
-
-
-// struct EvaluateService: EvaluateServiceProtocol {
-    
-//     func evaluate(request: Request, evaluateRequest: EvaluateRequest) throws -> EventLoopFuture<EvaluateResponseDto> {
-//         return try request.authorizedUser().flatMap { user in
-//             let intVersionWithPadding = self.transformVersionToIntWithPadding(version: evaluateRequest.version)
-//             let evaluate = Evaluate(id: evaluateRequest.id, rate: evaluateRequest.rate, versionSO: evaluateRequest.versionSO, version: intVersionWithPadding, userID: evaluateRequest.userID)
-            
-//             return Evaluate.query(on: request).filter((\.version == evaluate.version)).filter(\.userID == user.id).first().flatMap { existingEvaluate in
-//                 if var updatedEvaluate = existingEvaluate {
-//                     updatedEvaluate.rate = evaluate.rate
-//                     return updatedEvaluate.update(on: request).transform(to: EvaluateResponseDto.init(message: "Avaliação atualizada com sucesso!", evaluated: true))
-                    
-//                 }
-//                 var evaluateModel = evaluate
-//                 evaluateModel.userID = user.id
-//                 return evaluateModel.save(on: request).transform(to: EvaluateResponseDto.init(message: "Avaliação salva com sucesso!", evaluated: true))
-//                 }
-//         }
-//     }
-    
-//     func transformVersionToIntWithPadding(version: String) -> Int {
-//         let versionArray = version.components(separatedBy: ".")
-//         var versionWithPadding = ""
-//         for versionNumbers in versionArray {
-//             let versionPadding = String(format: "%04d",Int(versionNumbers) ?? 0)
-//             versionWithPadding.append(versionPadding)
-//         }
-//         return Int(versionWithPadding) ?? 0
-//     }
-
-// }
 
 }
