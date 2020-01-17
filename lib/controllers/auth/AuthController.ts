41d53c0088c352d1dd488a835bdc5c5c9aec534e
AleAlves <aleson.a.s@gmail.com>
2020-01-16 22:25:14 -0300

    jwt stuff and other stuff

--- lib/controllers/auth/AuthController.ts
+++ lib/controllers/auth/AuthController.ts
@@ -5,8 +5,9 @@ import { CryptoTools } from "../../security/CryptoTools";
 import { PublicKey } from "../../security/RSA/model/PublicKey";
 import { JWTSession } from "../../security/JWT/model/JWTSession";
 import { JWTModel } from "../../security/JWT/model/JWTModel";
+import { JWTType } from "../../security/JWT/model/JWTType"
 
-export class AuthController extends BaseController {   
+export class AuthController extends BaseController {
 
     public getPublicKey(req: Request, res: Response) {
         let publicKey = new PublicKey(CryptoTools.RSA().publicKey())
@@ -19,18 +20,18 @@ export class AuthController extends BaseController {
         super.send(res, CryptoTools.RSA().decrypt(data.teste, "utf8"))
     }
 
-    public generateAccessToken(req: Request, res: Response){
-        
+    public generateAccessToken(req: Request, res: Response) {
+
         let body = JSON.parse(JSON.stringify(req.body))
         let plainData = CryptoTools.RSA().decrypt(req.body.data, "json")
-        let session = new JWTSession(plainData)
+        let session = new JWTSession(plainData, JWTType.ACCESS)
         let encrypted = CryptoTools.JWT().signAccessToken(session)
         let accessToken = JSON.parse(JSON.stringify(new JWTModel(undefined, encrypted)))
 
-        console.log("Body: "+JSON.stringify(body))
-        console.log("Plain: "+JSON.stringify(plainData))
-        console.log("Session: "+JSON.stringify(plainData))
-        console.log("Token: "+ JSON.stringify(accessToken))
+        console.log("Body: " + JSON.stringify(body))
+        console.log("Plain: " + JSON.stringify(plainData))
+        console.log("Session: " + JSON.stringify(plainData))
+        console.log("Token: " + JSON.stringify(accessToken))
 
         super.send(res, accessToken)
     }
