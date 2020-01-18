886c87157c343ef99b0682fef3eea6d2783470f1
AleAlves <aleson.a.s@gmail.com>
2020-01-18 14:50:02 -0300

    8084

--- lib/server.ts
+++ lib/server.ts
@@ -1,6 +1,6 @@
 // lib/server.ts
 import app from "./app";
-const PORT = process.env.PORT || 808;
+const PORT = process.env.PORT || 8084;
 
 app.listen(PORT, () => {
     console.log('Express server listening on port ' + PORT);
