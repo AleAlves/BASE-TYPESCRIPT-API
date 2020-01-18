55bf612f1b21fb0f8639d97e95bb2c49a98510ae
AleAlves <aleson.a.s@gmail.com>
2020-01-18 14:49:25 -0300

    reverse

--- lib/server.ts
+++ lib/server.ts
@@ -1,15 +1,7 @@
 // lib/server.ts
 import app from "./app";
-import * as https from 'https';
-import * as fs from 'fs';
+const PORT = process.env.PORT || 808;
 
-const PORT = process.env.PORT || 8084;
-
-// Test only
-const httpsOptions = {
-    key: fs.readFileSync("key.pem"),
-    cert: fs.readFileSync("certificate.pem")
-}
-https.createServer(httpsOptions, app).listen(PORT, () => {
+app.listen(PORT, () => {
     console.log('Express server listening on port ' + PORT);
 })
